bdaa7113dbd792ac0af3268cae6d70e0761ecc26
mana.urano <manaurano@gmail.com>
2025-03-17 21:55:34 +0900

    野球チームの成績管理

--- src/baseball/BaseBallTeam.java
+++ src/baseball/BaseBallTeam.java
@@ -1,11 +1,26 @@
 package baseball;
 
-class BaseBallTeam {
-    // フィールド
-    String name;     // 球団名
-    int win;         // 勝ち
-    int lose;        // 負け
-    int draw;        // 引き分け
+public class BaseBallTeam {
+    private String name;
+    private int win;
+    private int lose;
+    private int draw;
 
+    // コンストラクタ
+    public BaseBallTeam(String name, int win, int lose, int draw) {
+        this.name = name;
+        this.win = win;
+        this.lose = lose;
+        this.draw = draw;
+    }
+
+    // 勝率を計算するメソッド
+    public double getRate() {
+        return (double) win / (win + lose);
+    }
+
+    // 成績を表示するメソッド
+    public void report() {
+        System.out.println(name + "の2022年の成績は" + win + "勝" + lose + "敗" + draw + "分、勝率は" + getRate() + "です。");
+    }
 }
-
--- src/baseball/Review03.java
+++ src/baseball/Review03.java
@@ -1,10 +1,20 @@
 package baseball;
 
 public class Review03 {
-
     public static void main(String[] args) {
-        // TODO 自動生成されたメソッド・スタブ
-//
-    }
+        // 各チームのインスタンスを作成
+        BaseBallTeam[] teams = {
+            new BaseBallTeam("東京ヤクルトスワローズ", 80, 59, 4),
+            new BaseBallTeam("横浜DeNAベイスターズ", 73, 68, 2),
+            new BaseBallTeam("阪神タイガース", 68, 71, 4),
+            new BaseBallTeam("読売ジャイアンツ", 68, 72, 3),
+            new BaseBallTeam("広島東洋カープ", 66, 74, 3),
+            new BaseBallTeam("中日ドラゴンズ", 66, 75, 2)
+        };
 
+        // 各チームの成績を表示
+        for (BaseBallTeam team : teams) {
+            team.report();
+        }
+    }
 }
